73be0f759ba942c50dc33a5834e0b970c86b3830
Mylena <myholiveira6@gmail.com>
2023-03-29 11:16:04 -0300

    finalização do projeto astro

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,10 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
 
-export async function get() {
-  return rss({
+export const get = () =>
+  rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: "https://my-blog-site.netlify.app",
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
-    customData: `<language>pt-br</language>`,
+    items: import.meta.glob("./**/*.md"),
+    customData: `<language>en-us</language>`,
   });
-}
